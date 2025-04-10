637229fbc294d3b368fe96bb8ccc8ae101de3687
danek52 <guskir3565@gmail.com>
2025-04-10 23:03:35 +0300

    add socket support to pages

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,11 +1,83 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import {useParams} from "react-router-dom";
+import axios from "axios";
 
 const ChatPage = () => {
+    const params = useParams();
+    const [messages, setMessages] = useState([]);
+    const [userInput, setUserInput] = useState("");
+    const [socket, setSocket] = useState(null);
+    const messagesEndRef = useRef(null);
+
+    const sendUserMessage = async () => {
+        console.log(userInput);
+        console.log('send message')
+        socket.send(userInput);
+    }
+
+
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({behavior: "smooth"});
+    };
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [messages]);
+
+    useEffect(() => {
+        const localSocket = new WebSocket(`ws://localhost:8000/ws/${params.chat_name}`)
+
+        localSocket.onopen = () => {
+            localSocket.send(localStorage.getItem("token"))
+        }
+        localSocket.onmessage = (e) => {
+            let messagesList = JSON.parse(e.data);
+            console.log("ws data", messagesList);
+            // console.log(typeof messagesList);
+            // console.log(messagesList[0]);
+            if (messagesList.length === 1) {
+                setMessages((prev) => [...prev, messagesList[0]]);
+            } else {
+                setMessages(messagesList);
+            }
+
+        }
+        setSocket(localSocket);
+        return () => {
+            localSocket.close(); // Важно закрывать соединение при размонтировании!
+        };
+
+    }, [params.chat_name]);
+    console.log('msg2', messages);
+
     return (
         <div>
             <h1>
                 Messages here
             </h1>
+            <ul className="flex gap-3 flex-col my-4 h-[50vh] overflow-auto inset-shadow-indigo-200 p-5 px-8 border-blue-300 border-2">
+                {messages.map((message, index) => (
+                    <li key={index}>
+                        <div className="flex gap-2">
+                            <div className="bg-emerald-300 px-3 py-1 rounded-2xl">
+                                {message.message}
+                            </div>
+                            <div className="text-gray-400">
+                                user: {message.user_phone}
+                            </div>
+                        </div>
+                    </li>
+                ))}
+                <div ref={messagesEndRef}/>
+
+            </ul>
+
+            <div className="flex gap-2 px-8">
+                <input type="text"
+                       onChange={(event) => setUserInput(event.target.value)}
+                       className="w-60 px-1 py-2 rounded-md border-1 outline-blue-500 border-blue-500 "/>
+                <button onClick={sendUserMessage}>Send</button>
+            </div>
         </div>
     );
 };
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -4,13 +4,14 @@ const host = "http://127.0.0.1:8000"
 
 const MainPage = () => {
     const [chats, setChats] = useState([])
+    const [chatName, setChatName] = useState('')
 
     useEffect(() => {
         const getChats = async () => {
             let chats = await fetch(`${host}/chats`);
             chats = await chats.json()
             chats = chats['chats']
-            console.log(chats)
+            console.log("cc", chats)
             setChats(chats)
             return chats
         }
@@ -22,14 +23,15 @@ const MainPage = () => {
     return (
         <>
             <h1>All chats</h1>
+            <input type="text" onChange={(event) => setChatName(event.target.value)}/>
+            <button> Create new chat</button>
             {/*<button onClick={getChats}></button>*/}
             <div>
-                {chats.map((chat) => (
-                    <li key={chat.chat_name}>
-                        <a href={`/chats/${chat.chat_name}`}>
-                            {chat.chat_name}
+                {Object.keys(chats).map((chat) => (
+                    <li key={chats[chat]}>
+                        <a href={`/chats/${chat}`}>
+                            {chat}
                         </a>
-
                     </li>
                 ))}
             </div>
