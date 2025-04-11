4e818b72e41e773b651a7b239da2ff1242bcba8a
danek52 <guskir3565@gmail.com>
2025-04-11 15:40:14 +0300

    create docker files, update main for server

--- backend_src/main.py
+++ backend_src/main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request
@@ -8,14 +9,19 @@ from core.redis_conf import init_redis, close_redis
 from database import async_session
 from modules.chat.services import fill_users_sockets
 from routes import main_router
+from core.config import config
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.ERROR,
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     )
+    try:
+        os.makedirs(os.path.join(config.base_dir, "uploads"))
+    except FileExistsError:
+        pass
     init_redis()
     await init_models()
     await fill_users_sockets()
@@ -30,7 +36,7 @@ app = FastAPI(
 app.include_router(main_router)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://localhost", "http://localhost:80"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
